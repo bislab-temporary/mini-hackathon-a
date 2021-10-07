5bb0469aebc40a4ed175257b367b0918be892245
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-07 11:26:56 +0900

    refactor: 可読性をあげるため
    ボタンを別コンポネーントに切り出した

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from './components/Button';
 
 const App = () => {
+  const [count, setCount] = useState<number>(0);
+
   return (
     <>
       <h1>カウンターアプリ</h1>
-      <div>0</div>
-      <button>アップ</button>
-      <button>10アップ</button>
-      <button>ダウン</button>
-      <button>10ダウン</button>
-      <button>リセット</button>
-      <div>0なんですけど</div>
+      <div>{count}</div>
+      <Button label="アップ" onClick={() => setCount((prevState) => prevState + 1)} />
+      <Button label="10アップ" onClick={() => setCount((prevState) => prevState + 10)} />
+      <Button label="ダウン" onClick={() => setCount((prevState) => prevState - 1)} />
+      <Button label="10ダウン" onClick={() => setCount((prevState) => prevState - 10)} />
+      <Button label="リセット" onClick={() => setCount(0)} />
+      {count === 0 && <div>0なんですけど</div>}
     </>
   );
 };
