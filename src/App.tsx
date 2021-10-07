fd530bf89da7ea7b5b520b4d3496f0bcd6c61150
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-07 11:38:43 +0900

    refactor: メンテナンス性をあげるため
    関数を抽象化した

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import Button from './components/Button';
 const App = () => {
   const [count, setCount] = useState<number>(0);
 
+  const updateCounter = (num: number) => {
+    setCount((prevState) => prevState + num);
+  };
+
   return (
     <>
       <h1>カウンターアプリ</h1>
       <div>{count}</div>
-      <Button label="アップ" onClick={() => setCount((prevState) => prevState + 1)} />
-      <Button label="10アップ" onClick={() => setCount((prevState) => prevState + 10)} />
-      <Button label="ダウン" onClick={() => setCount((prevState) => prevState - 1)} />
-      <Button label="10ダウン" onClick={() => setCount((prevState) => prevState - 10)} />
-      <Button label="リセット" onClick={() => setCount(0)} />
+      <Button label="アップ" onClick={() => updateCounter(1)} />
+      <Button label="10アップ" onClick={() => updateCounter(10)} />
+      <Button label="ダウン" onClick={() => updateCounter(-1)} />
+      <Button label="10ダウン" onClick={() => updateCounter(-10)} />
+      <Button label="リセット" onClick={() => updateCounter(-count)} />
       {count === 0 && <div>0なんですけど</div>}
     </>
   );
