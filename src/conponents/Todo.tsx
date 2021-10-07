22b79d19030518ce6a8ebf8108d8152f0035aec7
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-07 15:44:16 +0900

    feat: 削除ボタンの追加
    削除機能を追加するため

--- src/conponents/Todo.tsx
+++ src/conponents/Todo.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 
-type Props = {
+type TodoType = {
   title: string;
 };
 
-const Todo = ({ title }: Props) => {
+type Props = {
+  index: number;
+  title: string;
+  todos: TodoType[];
+  setTodos: Dispatch<SetStateAction<TodoType[]>>;
+};
+
+const Todo = ({ index, title, todos, setTodos }: Props) => {
+  const removeTodo = () => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <p>{title}</p>
-      <button style={{ border: '1px solid gray', padding: '0.2rem', borderRadius: '5px' }}>
+      <button
+        onClick={() => removeTodo()}
+        style={{ border: '1px solid gray', padding: '0.2rem', borderRadius: '5px' }}
+      >
         削除する
       </button>
     </>
