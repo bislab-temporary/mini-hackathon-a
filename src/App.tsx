79415c898accd5e606abdff822cd83d0c90a764a
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-07 09:53:33 +0900

    feat: htmlからの書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <h1>カウンターアプリ</h1>
+      <div>0</div>
+      <button>アップ</button>
+      <button>10アップ</button>
+      <button>ダウン</button>
+      <button>10ダウン</button>
+      <button>リセット</button>
+      <div>0なんですけど</div>
+    </>
+  );
 };
 
 export default App;
