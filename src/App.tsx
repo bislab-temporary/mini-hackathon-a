6efec3e68385bf8479cd3554b3429c321a35e888
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-10-07 14:13:32 +0900

    feat: 入力フォームを追加するため
    フォームで入力された情報を管理するため

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
 import Todo from './conponents/Todo';
 
 const todos = [{ title: '資料を作る' }, { title: '歯医者に行く' }, { title: '課題をやる' }];
 
 const App = () => {
+  const [title, setTitle] = useState<string>('');
+
   return (
     <>
+      <input onChange={(event: ChangeEvent<HTMLInputElement>) => setTitle(event.target.value)} />
       {todos.map((todo) => (
         <Todo key={todo.title} title={todo.title} />
       ))}
